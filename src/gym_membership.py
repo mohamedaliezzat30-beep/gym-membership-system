6a3d725eb6912e4ab8e5cf227508aea498a75a5e
mohamedaliezzat30-beep <mohamedaliezzat30@gmail.com>
2025-11-17 17:55:31 +0000

    started my gym system class, just a basic setup

--- src/gym_membership.py
+++ src/gym_membership.py
@@ -1,1 +1,9 @@
 # Gym Membership System CW1 start
+
+
+# I'm starting my Gym System class, just making the structure for now
+
+class GymSystem:
+    def __init__(self):
+        self.members = {}   # That's gonna store members here later
+        self.next_id = 1    # That's for auto IDs
